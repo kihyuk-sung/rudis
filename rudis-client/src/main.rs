0fc43b945da048a5fe727d56ef64ea83f4e2a4a1
K <kihyuk.sung@outlook.kr>
2023-02-03 23:03:56 +0900

    feat: write_data in client
    - duplicate in server

--- rudis-client/src/main.rs
+++ rudis-client/src/main.rs
@@ -1,9 +1,17 @@
-use std::net::TcpStream;
+use std::{net::TcpStream, io::{Write, Result}};
 
 fn main() {
     let addr = "127.0.0.1:1234";
-    match TcpStream::connect(addr) {
+    match TcpStream::connect(addr)
+        .and_then(write_data) {
             Ok(_) => println!("ok"),
             Err(_) => println!("not ok"),
         }
+}
+
+fn write_data(mut stream: TcpStream) -> Result<TcpStream> {
+    match stream.write("hello".as_bytes()) {
+        Ok(_) => Ok(stream),
+        Err(e) => Err(e),
+    }
 }
