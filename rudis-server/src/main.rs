d332d12098eaf8184e214e297ac470a8b3c4add7
K <kihyuk.sung@outlook.kr>
2023-02-03 19:51:36 +0900

    feat: bind tcp
    - if binding address is fail, then print some message,
    not panic.
    - because this error can handle, and can show great
    error message.

--- rudis-server/src/main.rs
+++ rudis-server/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-- rudis server --");
+
+    let address = "0.0.0.0:1234";
+    match TcpListener::bind(address) {
+        Ok(_) => println!("bind {address}"),
+        Err(_) => println!("cannot bind {address}"),
+    }
 }
