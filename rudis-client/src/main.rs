3e8193d7ba80f1f4d5200209084769d1535c7388
K <kihyuk.sung@outlook.kr>
2023-02-03 23:06:32 +0900

    feat: read_data in client

--- rudis-client/src/main.rs
+++ rudis-client/src/main.rs
@@ -1,9 +1,10 @@
-use std::{net::TcpStream, io::{Write, Result}};
+use std::{net::TcpStream, io::{Write, Result, Read}};
 
 fn main() {
     let addr = "127.0.0.1:1234";
     match TcpStream::connect(addr)
-        .and_then(write_data) {
+        .and_then(write_data)
+        .and_then(read_data) {
             Ok(_) => println!("ok"),
             Err(_) => println!("not ok"),
         }
@@ -13,5 +14,13 @@ fn write_data(mut stream: TcpStream) -> Result<TcpStream> {
     match stream.write("hello".as_bytes()) {
         Ok(_) => Ok(stream),
         Err(e) => Err(e),
+    }
+}
+
+fn read_data(mut stream: TcpStream) -> Result<(TcpStream, [u8; 64])> {
+    let mut buf = [0; 64];
+    match stream.read(&mut buf) {
+        Ok(_) => Ok((stream, buf)),
+        Err(_) => todo!(),
     }
 }
