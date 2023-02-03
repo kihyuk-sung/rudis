1a44f8567827e2c7a954469ed1b93973293cb7f1
K <kihyuk.sung@outlook.kr>
2023-02-03 22:57:23 +0900

    feat: write_data
    - write data "world" to client

--- rudis-server/src/main.rs
+++ rudis-server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream, SocketAddr}, io::{Result, Read}, str::from_utf8};
+use std::{net::{TcpListener, TcpStream, SocketAddr}, io::{Result, Read, Write}};
 
 fn main() {
     println!("-- rudis server --");
@@ -8,7 +8,8 @@ fn main() {
     match TcpListener::bind(address)
         .and_then(|l| l.accept())
         .and_then(read_data)
-        .and_then(print_data) {
+        .and_then(print_data)
+        .and_then(write_data) {
             Ok(_) => println!("ok"),
             Err(e) => println!("{}", e),
         }
@@ -25,4 +26,8 @@ fn read_data((mut stream, _addr): (TcpStream, SocketAddr)) -> Result<(TcpStream,
 fn print_data((stream, data): (TcpStream, [u8; 64])) -> Result<TcpStream> {
     println!("{:?}", data);
     Ok(stream)
+}
+
+fn write_data(mut stream: TcpStream) -> Result<usize> {
+    stream.write("world".as_bytes())
 }
