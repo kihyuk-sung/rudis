eaf477786e4066cd5ceac73b9a433a9725f22984
K <kihyuk.sung@outlook.kr>
2023-02-03 23:00:11 +0900

    feat: client connect

--- rudis-client/src/main.rs
+++ rudis-client/src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:1234";
+    match TcpStream::connect(addr) {
+            Ok(_) => println!("ok"),
+            Err(_) => println!("not ok"),
+        }
 }
