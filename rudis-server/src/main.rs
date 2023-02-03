fb40a5a643e57238c19f25b338210056bfd83df7
K <kihyuk.sung@outlook.kr>
2023-02-03 22:43:54 +0900

    feat: read_data from stream
    - read data once and return it

--- rudis-server/src/main.rs
+++ rudis-server/src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream, SocketAddr}, io::{Result, Read}};
 
 fn main() {
     println!("-- rudis server --");
@@ -6,8 +6,17 @@ fn main() {
     let address = "0.0.0.0:1234";
 
     match TcpListener::bind(address)
-        .and_then(|l| l.accept()) {
+        .and_then(|l| l.accept())
+        .and_then(read_data) {
             Ok(_) => println!("ok"),
             Err(e) => println!("{}", e),
         }
+}
+
+fn read_data((mut stream, _addr): (TcpStream, SocketAddr)) -> Result<(TcpStream, [u8; 64])> {
+    let mut buf = [0; 64];
+    match stream.read(&mut buf) {
+        Ok(_) => Ok((stream, buf)),
+        Err(_) => todo!(),
+    }
 }
