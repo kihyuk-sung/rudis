3c9e25d8283a285dc0ec40bbfe01721fae728122
K <kihyuk.sung@outlook.kr>
2023-02-04 15:17:46 +0900

    feat: rudis::stream
    - read_data
    - write_data
    - print_data
    - to remove duplicate code in server / client

---
+++ rudis/src/stream.rs
@@ -0,0 +1,21 @@
+use std::{net::TcpStream, io::{Result, Read, Write}, str::from_utf8};
+
+pub fn read_data(mut stream: TcpStream) -> Result<(TcpStream, [u8; 64])> {
+    let mut buf = [0; 64];
+    match stream.read(&mut buf) {
+        Ok(_) => Ok((stream, buf)),
+        Err(_) => todo!(),
+    }
+}
+
+pub fn write_data(mut stream: TcpStream, data: &[u8]) -> Result<TcpStream> {
+    match stream.write(data) {
+        Ok(_) => Ok(stream),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn print_data((stream, data): (TcpStream, [u8; 64])) -> Result<TcpStream> {
+    println!("{}", from_utf8(&data).unwrap());
+    Ok(stream)
+}
