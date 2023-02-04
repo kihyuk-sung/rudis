a5d01cc282d5150fabecf66e54c58f941502b802
K <kihyuk.sung@outlook.kr>
2023-02-04 15:18:50 +0900

    feat: use rudis::stream in client

--- rudis_client/src/main.rs
+++ rudis_client/src/main.rs
@@ -1,32 +1,13 @@
-use std::{net::TcpStream, io::{Write, Result, Read}, str::from_utf8};
+use std::{net::TcpStream};
+use rudis::stream::{read_data, write_data, print_data};
 
 fn main() {
     let addr = "127.0.0.1:1234";
     match TcpStream::connect(addr)
-        .and_then(write_data)
+        .and_then(|stream| write_data(stream, "hello".as_bytes()))
         .and_then(read_data)
         .and_then(print_data) {
             Ok(_) => println!("ok"),
             Err(_) => println!("not ok"),
         }
-}
-
-fn write_data(mut stream: TcpStream) -> Result<TcpStream> {
-    match stream.write("hello".as_bytes()) {
-        Ok(_) => Ok(stream),
-        Err(e) => Err(e),
-    }
-}
-
-fn read_data(mut stream: TcpStream) -> Result<(TcpStream, [u8; 64])> {
-    let mut buf = [0; 64];
-    match stream.read(&mut buf) {
-        Ok(_) => Ok((stream, buf)),
-        Err(_) => todo!(),
-    }
-}
-
-fn print_data((stream, data): (TcpStream, [u8; 64])) -> Result<TcpStream> {
-    println!("{}", from_utf8(&data).unwrap());
-    Ok(stream)
 }
