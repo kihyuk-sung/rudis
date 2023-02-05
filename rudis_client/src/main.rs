dd73cacef0ba6e1d630719a1493a25f152868687
K <kihyuk.sung@outlook.kr>
2023-02-05 19:43:28 +0900

    feat: server <-> client protocol
    - server can handle one client request infinite
    - client send 3 times
    - server and client echo each other

--- rudis_client/src/main.rs
+++ rudis_client/src/main.rs
@@ -1,13 +1,43 @@
-use std::{net::TcpStream};
-use rudis::stream::{read_data, write_data, print_data};
+use std::{net::TcpStream, io::{Result, Read}, str::from_utf8};
+use rudis::stream::{write_data, read_full};
 
 fn main() {
     let addr = "127.0.0.1:1234";
     match TcpStream::connect(addr)
-        .and_then(|stream| write_data(stream, "hello".as_bytes()))
-        .and_then(read_data)
-        .and_then(print_data) {
+        .and_then(|stream| query(stream, "hello1"))
+        .and_then(|stream| query(stream, "hello2"))
+        .and_then(|stream| query(stream, "hello3")) {
             Ok(_) => println!("ok"),
-            Err(_) => println!("not ok"),
+            Err(e) => println!("not ok {e}"),
         }
+}
+
+fn query(stream: TcpStream, data: &str) -> Result<TcpStream> {
+    let mut buf = [0; 64];
+    
+    match write_data(stream, &data.len().to_be_bytes()[4..8])
+        .and_then(|stream| write_data(stream, data.as_bytes()))
+        .and_then(|stream| read_full(stream, &mut buf[0..4]))
+        .and_then(data_len)
+        .and_then(|(mut stream, len)| match stream.read_exact(&mut buf[4..4 + len]) {
+            Ok(()) => Ok((stream, len)),
+            Err(e) => Err(e),
+        })
+        .and_then(|(stream, l)| match from_utf8(&buf[4.. 4 + l]) {
+            Ok(d) => Ok((stream, d)),
+            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
+        })
+        .and_then(|(stream, d)| {
+            println!("server sent: {d}");
+            Ok(stream)
+        }) {
+            Ok(stream) => Ok(stream),
+            Err(e) => Err(e),
+        }
+}
+
+fn data_len((stream, buf): (TcpStream, &[u8])) -> Result<(TcpStream, usize)> {
+    Ok(
+        (stream, usize::from_be_bytes([0, 0, 0, 0, buf[0], buf[1], buf[2], buf[3]]))
+    )
 }
--- rudis_server/src/main.rs
+++ rudis_server/src/main.rs
@@ -1,6 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::Result, str::from_utf8};
-
-use rudis::stream::{read_full, write_data};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, str::from_utf8};
 
 fn main() {
     println!("-- rudis server --");
@@ -22,25 +20,43 @@ fn main() {
 
 const MAX_SIZE: usize = 4096;
 fn accept(listener: &TcpListener) {
-    let mut buf = [0; 4 + MAX_SIZE];
     match listener.accept()
-        .and_then(|(stream, _addr)| read_full(stream, &mut buf[0..4]))
-        .and_then(data_len)
-        .and_then(|(stream, len)| read_full(stream, &mut buf[4..4 + len]))
-        .and_then(print_data)
-        .and_then(|(stream, buf)| write_data(stream, buf)) {
-            Ok(_) => (),
-            Err(_) => (),
+    .and_then(|(s, _)| Ok(one_request(s))) {
+        Ok(()) => (),
+        Err(_) => (),
+    }
+}
+
+fn one_request(mut stream: TcpStream) {
+    let mut buf = [0; 4 + MAX_SIZE];
+    loop {
+        match stream.read_exact(&mut buf[0..4])
+        .and_then(|()| Ok(len(&buf[0..4])))
+        .and_then(|l| match stream.read_exact(&mut buf[4..4 + l]) {
+            Ok(()) => Ok(l),
+            Err(e) => Err(e),
+        })
+        .and_then(|l| match from_utf8(&buf[4.. 4 + l]) {
+            Ok(d) => Ok(d),
+            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
+        })
+        .and_then(|d| {
+            println!("clent send: {d}");
+            Ok(d)
+        })
+        .and_then(|d| {
+            match stream.write_all(&d.len().to_be_bytes()[4..8]) {
+                Ok(()) => Ok(d),
+                Err(e) => Err(e),
+            }
+        })
+        .and_then(|d| stream.write_all(d.as_bytes())) {
+            Ok(()) => (),
+            Err(_) => return,
         }
+    }
 }
 
-fn data_len((stream, buf): (TcpStream, &[u8])) -> Result<(TcpStream, usize)> {
-    Ok(
-        (stream, usize::from_be_bytes([0, 0, 0, 0, buf[0], buf[1], buf[2], buf[3]]))
-    )
-}
-
-pub fn print_data((stream, data): (TcpStream, &[u8])) -> Result<(TcpStream, &[u8])> {
-    println!("{}", from_utf8(data).unwrap());
-    Ok((stream, data))
+fn len(len: &[u8]) -> usize {
+    usize::from_be_bytes([0, 0, 0, 0, len[0], len[1], len[2], len[3]])
 }
