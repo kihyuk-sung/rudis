533cc5f329b91507c7b764adc65529fb27e5b9e7
K <kihyuk.sung@outlook.kr>
2023-02-06 20:03:19 +0900

    feat: change server
    - client와 같은 이유로 서버 변경
    - client에서 연결 끊으면 UnexpectedEof 에러 발생

--- rudis_server/src/main.rs
+++ rudis_server/src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, str::from_utf8};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write, Result}};
 
 fn main() {
     println!("-- rudis server --");
@@ -14,49 +14,40 @@ fn main() {
     };
 
     loop {
-        accept(&listener);
-    }
-}
-
-const MAX_SIZE: usize = 4096;
-fn accept(listener: &TcpListener) {
-    match listener.accept()
-    .and_then(|(s, _)| Ok(one_request(s))) {
-        Ok(()) => (),
-        Err(_) => (),
-    }
-}
-
-fn one_request(mut stream: TcpStream) {
-    let mut buf = [0; 4 + MAX_SIZE];
-    loop {
-        match stream.read_exact(&mut buf[0..4])
-        .and_then(|()| Ok(len(&buf[0..4])))
-        .and_then(|l| match stream.read_exact(&mut buf[4..4 + l]) {
-            Ok(()) => Ok(l),
-            Err(e) => Err(e),
-        })
-        .and_then(|l| match from_utf8(&buf[4.. 4 + l]) {
-            Ok(d) => Ok(d),
-            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
-        })
-        .and_then(|d| {
-            println!("clent send: {d}");
-            Ok(d)
-        })
-        .and_then(|d| {
-            match stream.write_all(&d.len().to_be_bytes()[4..8]) {
-                Ok(()) => Ok(d),
-                Err(e) => Err(e),
-            }
-        })
-        .and_then(|d| stream.write_all(d.as_bytes())) {
+        match accept(&listener) {
             Ok(()) => (),
-            Err(_) => return,
+            // TODO: UnexpectedEof when client close connection
+            Err(_) => (),
         }
     }
 }
 
-fn len(len: &[u8]) -> usize {
-    usize::from_be_bytes([0, 0, 0, 0, len[0], len[1], len[2], len[3]])
+fn accept(listener: &TcpListener) -> Result<()> {
+    listener.accept().and_then(|(stream, _addr)| one_request(stream))
+}
+
+const LEN_OFFSET: usize = 4;
+const READ_BUF_SIZE: usize = 4096;
+fn one_request(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0; LEN_OFFSET + READ_BUF_SIZE];
+    loop {
+        one_query(&mut stream, &mut buf)?;
+    }
+}
+
+fn one_query(stream: &mut TcpStream, buf: &mut [u8]) -> Result<()> {
+    stream.read_exact(&mut buf[0..LEN_OFFSET])?;
+    let read_len: usize = u32::from_be_bytes(buf[0..LEN_OFFSET].try_into().unwrap()) as usize;
+    stream.read_exact(&mut buf[LEN_OFFSET..LEN_OFFSET + read_len])?;
+
+    let data = match std::str::from_utf8(&buf[LEN_OFFSET..LEN_OFFSET + read_len]) {
+        Ok(s) => s,
+        Err(_) => "error data",
+    };
+    println!("client sent: {data}");
+
+    stream.write_all(&("world".len() as u32).to_be_bytes())?;
+    stream.write_all(&("world").as_bytes())?;
+
+    Ok(())
 }
