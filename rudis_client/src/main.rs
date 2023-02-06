372369d25af30b0cf3560ec0ccd306c469197142
K <kihyuk.sung@outlook.kr>
2023-02-06 19:35:58 +0900

    feat: client 변경
    - query는 and_then chaining 아닌 형태로 변경
    - 이유는 and_then 으로 연결하는 부분이 필요 없는 부분들도 많은 것 같음.
    - Result의 ?; 형태로 변경
    - main 은 query 의 and 로 묶음
    - 에러 처리를 한 곳에서 하기 위함

--- rudis_client/src/main.rs
+++ rudis_client/src/main.rs
@@ -1,43 +1,38 @@
-use std::{net::TcpStream, io::{Result, Read}, str::from_utf8};
-use rudis::stream::{write_data, read_full};
+use std::{net::TcpStream, io::{Result, Read, Write}};
 
 fn main() {
     let addr = "127.0.0.1:1234";
-    match TcpStream::connect(addr)
-        .and_then(|stream| query(stream, "hello1"))
-        .and_then(|stream| query(stream, "hello2"))
-        .and_then(|stream| query(stream, "hello3")) {
-            Ok(_) => println!("ok"),
-            Err(e) => println!("not ok {e}"),
-        }
-}
-
-fn query(stream: TcpStream, data: &str) -> Result<TcpStream> {
-    let mut buf = [0; 64];
+    let mut stream = match TcpStream::connect(addr) {
+            Ok(s) => s,
+            Err(e) => panic!("{:?}", e),
+        };
     
-    match write_data(stream, &data.len().to_be_bytes()[4..8])
-        .and_then(|stream| write_data(stream, data.as_bytes()))
-        .and_then(|stream| read_full(stream, &mut buf[0..4]))
-        .and_then(data_len)
-        .and_then(|(mut stream, len)| match stream.read_exact(&mut buf[4..4 + len]) {
-            Ok(()) => Ok((stream, len)),
-            Err(e) => Err(e),
-        })
-        .and_then(|(stream, l)| match from_utf8(&buf[4.. 4 + l]) {
-            Ok(d) => Ok((stream, d)),
-            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
-        })
-        .and_then(|(stream, d)| {
-            println!("server sent: {d}");
-            Ok(stream)
-        }) {
-            Ok(stream) => Ok(stream),
-            Err(e) => Err(e),
-        }
+    match query(&mut stream, "hello1")
+        .and(query(&mut stream, "hello2"))
+        .and(query(&mut stream, "hello3")) {
+            Ok(()) => (),
+            Err(e) => eprintln!("error: {:?}", e),
+        };
 }
 
-fn data_len((stream, buf): (TcpStream, &[u8])) -> Result<(TcpStream, usize)> {
-    Ok(
-        (stream, usize::from_be_bytes([0, 0, 0, 0, buf[0], buf[1], buf[2], buf[3]]))
-    )
+const LEN_OFFSET: usize = 4;
+const READ_BUF_SIZE: usize = 4096;
+fn query(stream:&mut TcpStream, data: &str) -> Result<()> {
+    let mut buf = [0; LEN_OFFSET + READ_BUF_SIZE];
+
+    stream.write_all(&(data.len() as u32).to_be_bytes())?;
+    stream.write_all(&data.as_bytes())?;
+
+    stream.read_exact(&mut buf[0..LEN_OFFSET])?;
+    let read_len: usize = u32::from_be_bytes(buf[0..LEN_OFFSET].try_into().unwrap()) as usize;
+    stream.read_exact(&mut buf[LEN_OFFSET..LEN_OFFSET + read_len])?;
+    
+    let data = match std::str::from_utf8(&buf[LEN_OFFSET..LEN_OFFSET + read_len]) {
+        Ok(s) => s,
+        Err(_) => "error data",
+    };
+
+    println!("server sent: {data}");
+
+    Ok(())
 }
